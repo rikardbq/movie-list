2b691f28ca90de88067af455dd6cc209d663677c
rikardbq <j10rikardb@gmail.com>
2020-03-28 03:46:58 +0100

    remove conditional on route helmet to make it work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import AppHelmet from "./Helmets/AppHelmet";
+
 import AppRouter from "./Router/AppRouter";
 import Routes from "./Router/Routes";
 
--- src/Helmets/RouteHelmet.js
+++ src/Helmets/RouteHelmet.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-const RouteHelmet = ({ title, description, themeColor }) =>
-  title ||
-  description ||
-  (themeColor && (
-    <Helmet>
-      {title && <title>{title}</title>}
-      {description && <meta name="description" content={description} />}
-      {themeColor && <meta name="theme-color" content={themeColor} />}
-    </Helmet>
-  ));
+const RouteHelmet = ({ title, description, themeColor }) => (
+  <Helmet>
+    {title && <title>{title}</title>}
+    {description && <meta name="description" content={description} />}
+    {themeColor && <meta name="theme-color" content={themeColor} />}
+  </Helmet>
+);
 
 export default RouteHelmet;
